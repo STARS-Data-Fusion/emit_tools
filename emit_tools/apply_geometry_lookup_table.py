ea84fefa153e1dcfa6917232d664007d03a26f4b
Gregory Halverson <gregory.halverson@gmail.com>
2024-07-19 07:03:05 -0700

    breaking down steps of geometry lookup table application

--- emit_tools/apply_geometry_lookup_table.py
+++ emit_tools/apply_geometry_lookup_table.py
@@ -19,19 +19,35 @@ def apply_GLT(swath_array: np.ndarray, GLT_array: np.ndarray, fill_value: int = 
     if swath_array.ndim == 2:
         swath_array = swath_array[:, :, np.newaxis]
 
+    # ortho_array = np.full(
+    #     (GLT_array.shape[0], GLT_array.shape[1], swath_array.shape[-1]),
+    #     fill_value,
+    #     dtype=np.float32,
+    # )
+
+    # get the length of the latitude dimension from the first dimension of the geometry lookup table array
+    latitude_length = GLT_array.shape[0]
+    # get the length of the longitude dimension from the second dimension of the geometry lookup table array
+    longitude_length = GLT_array.shape[1]
+    # get the length of the band dimension from the last dimension of the swath array
+    band_length = swath_array.shape[-1]
+    # collect the shape of the orthorectified array
+    ortho_array_shape = (latitude_length, longitude_length, band_length)
+
+    # create an empty orthorectified array filled with the fill value
     ortho_array = np.full(
-        (GLT_array.shape[0], GLT_array.shape[1], swath_array.shape[-1]),
+        ortho_array_shape,
         fill_value,
         dtype=np.float32,
     )
 
+    # mask of where valid gometry lookup table values are
     valid_GLT = np.all(GLT_array != GLT_nodata_value, axis=-1)
 
-    # Adjust for One based Index - make a copy to prevent decrementing multiple times inside ortho_xr when applying the glt to elev
-    glt_array_copy = GLT_array.copy()
-    glt_array_copy[valid_GLT] -= 1
-    ortho_array[valid_GLT, :] = swath_array[
-        glt_array_copy[valid_GLT, 1], glt_array_copy[valid_GLT, 0], :
-    ]
+    # convert geometry lookup table indices to zero-based indices for numpy
+    zero_based_indices = GLT_array - 1  
+
+    # sample the swath array at the zero-based indices and assign to the orthorectified array
+    ortho_array[valid_GLT, :] = swath_array[zero_based_indices[valid_GLT, 1], zero_based_indices[valid_GLT, 0], :]
 
     return ortho_array
